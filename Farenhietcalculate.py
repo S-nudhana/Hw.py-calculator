b7267bf765cf8c7440e92a77590ba9fb517bd275
nuza1583 <nu37317@gmail.com>
2022-06-04 19:02:27 +0700

    initial commit

---
+++ Farenhietcalculate.py
@@ -0,0 +1,15 @@
+Dollar = int(input("Dollar here : "     ))
+Cent = int(input("Cent here : "     ))
+CENT = Cent * 0.01
+Sum = Dollar + CENT
+Baht = Dollar * 34
+stang = Cent * 34
+Sumbaht = Baht + stang
+Sumbaht = Sum * 34
+
+print("You have (dollar)",Dollar)
+print("You have (cent)",Cent)
+print("Total ",Sum)
+print("You have (baht)",Baht)
+print("You have (Stang)",stang)
+print("You have total (Baht)",Sumbaht)
